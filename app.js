44924600c81af1601c20964b592ae018e24c7eff
Sam Louloudis <samanthaleelouloudis@gmail.com>
2023-12-13 09:10:59 +0000

    stuff

--- app.js
+++ app.js
@@ -1,56 +1,45 @@
-const images = [
-    {
-        image: `https://images.unsplash.com/photo-1624668456438-275609251df9?q=80&w=2050&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`,
-        thumbnail: `https://images.unsplash.com/photo-1624668456438-275609251df9?q=80&w=2050&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`,
-        alt: `A beautiful crane about to take flight in the snow, taken in Tsurui, Hokkaido, Japan`,
-    },{
-        image: `https://images.unsplash.com/photo-1604608684575-0478ddb56d48?q=80&w=2033&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`,
-        thumbnail: `https://images.unsplash.com/photo-1604608684575-0478ddb56d48?q=80&w=2033&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`,
-        alt: `A lynx licking its paw in the snow`
-    },{
-        image: `https://images.unsplash.com/photo-1612957514614-9c5bc5ba245f?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`,
-        thumbnail: `https://images.unsplash.com/photo-1612957514614-9c5bc5ba245f?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`,
-        alt: `A brown dog standing at attention in the snow, warm light filtering behind it.`
-    },{
-        image: `https://images.unsplash.com/photo-1597732081142-329f64f5962f?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`,
-        thumbnail: `https://images.unsplash.com/photo-1597732081142-329f64f5962f?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`,
-        alt: `A brown dog standing at attention in the snow, warm light filtering behind it.`
-    },{
-        image: `https://images.unsplash.com/photo-1463436755683-3f805a9d1192?q=80&w=2948&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`,
-        thumbnail: `https://images.unsplash.com/photo-1463436755683-3f805a9d1192?q=80&w=2948&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`,
-        alt: `A brown dog standing at attention in the snow, warm light filtering behind it.`
-    }
-]
-
-
 let thumbContainer = document.getElementById('thumb-container')
 let currentImageIndex = 0
 const displayElem = document.getElementById('display')
+let form = document.getElementById('search-form')
+let searchBar = document.getElementById('search-bar')
+let images;
 
-// async
 function init() {
-    // await fetchImages()
-    console.log(images)
+    fetchImages()
+}
+
+async function fetchImages(query = 'japan') {
+    let response = await fetch(`https://api.unsplash.com/search/photos?page=1&query=${query}&client_id=${process.env.CLIENT_ID}`)
+    let data = await response.json();
+    images = data.results
     updateDisplayImage(images[currentImageIndex])
     createThumbnails()
 }
 
+// form stuff
+form.addEventListener('submit', function(event) {
+    event.preventDefault()
+    fetchImages(searchBar.value)
+})
+
 
 function createThumbnails() {
+    thumbContainer.innerHTML = ''
     for (let image of images) {
         let thumbImg = document.createElement('img')
 
-        thumbImg.setAttribute('src', image.image)
-        thumbImg.setAttribute('alt', image.alt)
+        thumbImg.setAttribute('src', image.urls.thumb)
+        thumbImg.setAttribute('alt', image.alt_description)
         thumbImg.setAttribute('tabindex', '0')
         thumbImg.classList.add('thumb-image')
         thumbContainer.appendChild(thumbImg)
         thumbImg.addEventListener('click', function () {
             updateDisplayImage(image)
-            document.getElementById('announcer').textContent = image.alt
+            document.getElementById('announcer').textContent = image.alt_description
         })
         thumbImg.addEventListener('keydown', function (event) {
-            document.getElementById('announcer').textContent = image.alt
+            document.getElementById('announcer').textContent = image.alt_description
             if (event.key === 'Enter') updateDisplayImage(image)
             
         })
@@ -62,7 +51,7 @@ function updateScrollBar(currentImage) {
     let thumbnails = thumbContainer.querySelectorAll('.thumb-image');
     let activeThumbnail;
     thumbnails.forEach(function (thumb) {
-        if(thumb.getAttribute('src') === currentImage.thumbnail) {
+        if(thumb.getAttribute('src') === currentImage.urls.thumb) {
             activeThumbnail = thumb;
         }
     });
@@ -91,10 +80,10 @@ function updateDisplayImage(image) {
         displayElem.appendChild(currentDisplayImage)
     }
  
-    currentDisplayImage.setAttribute('src', image.image)
-    currentDisplayImage.setAttribute('alt', image.alt)
+    currentDisplayImage.setAttribute('src', image.urls.full)
+    currentDisplayImage.setAttribute('alt', image.alt_description)
     updateScrollBar(image)
-    document.getElementById('announcer').textContent = image.alt
+    document.getElementById('announcer').textContent = image.alt_description
 }
 
 next.addEventListener('click', function() { selectNextImage(1) })
